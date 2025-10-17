b8327f84bd1728e3b6ebcc0e70326b3f3f1178c3
timka5555timka-hub <timka5555timka@gmail.com>
2025-10-18 03:36:39 +0500

    Create lab5_6.py

---
+++ lab5_6.py
@@ -0,0 +1,9 @@
+friends = ["Антон", "Сергей", "Иван", "Михаил"]
+
+heroes = ["Бэтмен", "Железный человек", "Тор", "Человек-паук"]
+
+
+
+for i in range(len(friends)):
+
+    print(f"{friends[i]} — {heroes[i]}")
